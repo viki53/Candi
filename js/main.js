600dbf4409a9f6c6e18d8d30a60435cfb04061dc
Corentin Hatte <contact@corentin-hatte.eu>
2016-11-13 02:08:52 +0100

    Improve templates

--- js/main.js
+++ js/main.js
@@ -1,28 +1,33 @@
 function main ($, $$) {
-	var source = $('#tpl-question').innerHTML;
-	var template = Handlebars.compile(source);
+	var tpl_question = Handlebars.compile($('#tpl-question').innerHTML);
+
+	var tpl_answer_radio = Handlebars.compile($('#tpl-answer-radio').innerHTML);
+
+	var tpl_answer_checkbox = Handlebars.compile($('#tpl-answer-checkbox').innerHTML);
 
 	var question = {
+		id: 'q1',
 		label: 'Test question',
 		type: 'radio',
 		choices: [
 			{
+				id: 'q1a1',
 				label: '#1'
 			},
 			{
+				id: 'q1a2',
 				label: 'Number two'
 			},
 			{
+				id: 'q1a3',
 				label: 'Numero tres'
 			}
 		]
 	};
 
-	var html = template(question);
-	console.log(source);
-	// console.log(template);
-	// console.log(html);
-	$('#chat-messages').innerHTML = html;
+	$('#chat-messages').innerHTML = tpl_question(question);
+	$('#chat-form').innerHTML = tpl_answer_radio(question);
+	$('#chat-form').innerHTML += tpl_answer_checkbox(question);
 };
 
 document.addEventListener('DOMContentLoaded', function () {
