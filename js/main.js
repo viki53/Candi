12b374f3a3e0036006ac6a4cd87525afed428b1c
Corentin Hatte <contact@corentin-hatte.eu>
2016-11-13 02:57:47 +0100

    Basic design + JS logic

--- js/main.js
+++ js/main.js
@@ -1,11 +1,7 @@
-function main ($, $$) {
-	var tpl_question = Handlebars.compile($('#tpl-question').innerHTML);
-
-	var tpl_answer_radio = Handlebars.compile($('#tpl-answer-radio').innerHTML);
-
-	var tpl_answer_checkbox = Handlebars.compile($('#tpl-answer-checkbox').innerHTML);
-
-	var question = {
+const $ = document.querySelector.bind(document);
+const $$ = document.querySelectorAll.bind(document);
+const QUESTIONS = [
+	{
 		id: 'q1',
 		label: 'Test question',
 		type: 'radio',
@@ -23,13 +19,50 @@ function main ($, $$) {
 				label: 'Numero tres'
 			}
 		]
-	};
+	}
+];
 
-	$('#chat-messages').innerHTML = tpl_question(question);
-	$('#chat-form').innerHTML = tpl_answer_radio(question);
-	$('#chat-form').innerHTML += tpl_answer_checkbox(question);
+const TPL_QUESTION = Handlebars.compile($('#tpl-question').innerHTML);
+
+const TPL_ANSWERS = {
+	radio: Handlebars.compile($('#tpl-answer-radio').innerHTML),
+	checkbox: Handlebars.compile($('#tpl-answer-checkbox').innerHTML)
 };
 
-document.addEventListener('DOMContentLoaded', function () {
-	main(document.querySelector.bind(document), document.querySelectorAll.bind(document));
+class QuestionManager {
+	constructor(questions) {
+		this._currentIndex = -1; // Current question index
+		this._questions = questions;
+
+		this.nextQuestion();
+	}
+
+	nextQuestion() {
+		console.log('nextQuestion');
+		if (!this._questions[this._currentIndex + 1]) {
+			throw new RangeError('No more questions');
+		}
+
+		this._currentIndex++; // Increase current question index
+
+		this.displayCurrentQuestion();
+	}
+
+	displayCurrentQuestion() {
+		console.log('displayCurrentQuestion');
+		let question = this._questions[this._currentIndex];
+
+		$('#chat-messages').innerHTML = TPL_QUESTION(question);
+
+		if (!TPL_ANSWERS[question.type]) {
+			return this.nextQuestion();
+		}
+		else {
+			$('#chat-form').innerHTML = TPL_ANSWERS[question.type](question);
+		}
+	}
+};
+
+document.addEventListener('DOMContentLoaded', function() {
+	const MANAGER = new QuestionManager(QUESTIONS);
 }, false);
